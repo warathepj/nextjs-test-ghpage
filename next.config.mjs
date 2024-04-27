280692bf2353c002942278cfe0feac48519e0f79
warathepj <80021220+warathepj@users.noreply.github.com>
2024-04-27 17:25:15 +0700

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    basePath: isProd ? '/nextjs-test-ghpage' : '',
     output: 'export',
     distDir: 'dist',
     images: {
