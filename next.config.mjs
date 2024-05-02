0cd150b8e13ff2b9a9e971f9cb61b41f4807706e
warathepj <80021220+warathepj@users.noreply.github.com>
2024-05-02 13:45:48 +0700

    add action.yml publish.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    basePath: isProd ? '/nextjs-test-ghpage' : '',
+    // basePath: isProd ? '/nextjs-test-ghpage' : '',
+    basePath: '/nextjs-test-ghpage',
     output: 'export',
     distDir: 'dist',
     images: {
